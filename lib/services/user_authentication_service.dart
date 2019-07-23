97651718a6fd75a1a86a6a808082a0782dc59c78
Michael Derzhavets <m.derzhavets@emg-soft.com>
2019-07-23 16:42:05 +0300

    Removed sftp credentials from logs

--- lib/services/user_authentication_service.dart
+++ lib/services/user_authentication_service.dart
@@ -48,13 +48,13 @@ class UserAuthenticationService {
     PrefsProvider.saveSftpUsername(credentials.username);
     PrefsProvider.saveSftpPath(credentials.root);
 
-    Log.info(
-        TAG,
-        "SFTP PARAMS \n -------------------- \n"
-        "HOST: ${credentials.host} \n"
-        "PORT: ${credentials.port} \n"
-        "USERNAME: ${credentials.username} \n"
-        "PASSWORD: ${credentials.password} \n"
-        "ROOT: ${credentials.root}");
+//    Log.info(
+//        TAG,
+//        "SFTP PARAMS \n -------------------- \n"
+//        "HOST: ${credentials.host} \n"
+//        "PORT: ${credentials.port} \n"
+//        "USERNAME: ${credentials.username} \n"
+//        "PASSWORD: ${credentials.password} \n"
+//        "ROOT: ${credentials.root}");
   }
 }

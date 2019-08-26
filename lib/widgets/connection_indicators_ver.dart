f2bef8bb6c10e45dc202f4f8c533225e8969b972
Michael Derzhavets <m.derzhavets@emg-soft.com>
2019-08-26 16:25:58 +0300

    Made led colors less ugly and pathetic

--- lib/widgets/connection_indicators_ver.dart
+++ lib/widgets/connection_indicators_ver.dart
@@ -4,12 +4,10 @@ import '../service_locator.dart';
 
 class AppbarConnectionIndicators extends StatefulWidget {
   @override
-  _AppbarConnectionIndicatorsState createState() =>
-      _AppbarConnectionIndicatorsState();
+  _AppbarConnectionIndicatorsState createState() => _AppbarConnectionIndicatorsState();
 }
 
-class _AppbarConnectionIndicatorsState
-    extends State<AppbarConnectionIndicators> {
+class _AppbarConnectionIndicatorsState extends State<AppbarConnectionIndicators> {
   static final _manager = sl<ConnectionIndicatorManager>();
 
   @override
@@ -44,10 +42,7 @@ class _AppbarConnectionIndicatorsState
 
   static const String _BT = "ind_bt";
   static const String _SFTP = "ind_sftp";
-  final Map<String, Stream> _streams = {
-    _BT: _manager.btLitStream,
-    _SFTP: _manager.sftpLitStream
-  };
+  final Map<String, Stream> _streams = {_BT: _manager.btLitStream, _SFTP: _manager.sftpLitStream};
 }
 
 class IndicatorPainter extends CustomPainter {
@@ -58,15 +53,33 @@ class IndicatorPainter extends CustomPainter {
 
   @override
   void paint(Canvas canvas, Size size) {
+    if (isLit) {
+      canvas.drawCircle(
+          Offset(3.0, 3.0),
+          6.0,
+          Paint()
+            ..style = PaintingStyle.fill
+            ..color = color);
+    } else {
+      canvas.drawCircle(
+          Offset(3.0, 3.0),
+          5.0,
+          Paint()
+            ..strokeWidth = 2
+            ..strokeJoin = StrokeJoin.bevel
+            ..style = PaintingStyle.stroke
+            ..color = color);
+    }
+
     // white background
-    canvas.drawCircle(
-        Offset(3.0, 3.0),
-        6.0,
-        Paint()
-          ..strokeWidth = 2
-          ..strokeJoin = StrokeJoin.bevel
-          ..style = isLit ? PaintingStyle.fill : PaintingStyle.stroke
-          ..color = color);
+//    canvas.drawCircle(
+//        Offset(3.0, 3.0),
+//        6.0,
+//        Paint()
+//          ..strokeWidth = 2
+//          ..strokeJoin = StrokeJoin.bevel
+//          ..style = isLit ? PaintingStyle.fill : PaintingStyle.stroke
+//          ..color = color);
 
     // inner circle
 //    canvas.drawCircle(

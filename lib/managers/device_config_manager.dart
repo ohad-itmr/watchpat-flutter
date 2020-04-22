ad9d00a8a72d5b494bbd42c144f9e80c067206dc
Michael Derzhavets <m.derzhavets@emg-soft.com>
2020-04-22 20:46:34 +0300

    More logging for pin issue

--- lib/managers/device_config_manager.dart
+++ lib/managers/device_config_manager.dart
@@ -2,6 +2,7 @@ import 'package:my_pat/generated/i18n.dart';
 import 'package:my_pat/managers/manager_base.dart';
 import 'package:my_pat/domain_model/device_config_payload.dart';
 import 'package:my_pat/service_locator.dart';
+import 'package:my_pat/utils/log/log.dart';
 
 class DeviceConfigManager extends ManagerBase {
   static const String TAG = 'DeviceConfigManager';
@@ -12,7 +13,17 @@ class DeviceConfigManager extends ManagerBase {
 
   DeviceConfigPayload get deviceConfig => _deviceConfig;
 
-  void setDeviceConfiguration(DeviceConfigPayload config) => _deviceConfig = config;
+  void setDeviceConfiguration(DeviceConfigPayload config, {bool force = false}) {
+    if (force) {
+      Log.info(TAG, 'Setting config block');
+      _deviceConfig = config;
+    } else if (_deviceConfig == null) {
+      Log.info(TAG, 'Setting config block');
+      _deviceConfig = config;
+    } else {
+      Log.info(TAG, 'Config block already exist');
+    }
+  }
 
   @override
   void dispose() {
